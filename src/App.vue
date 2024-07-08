fcdc25f1b984a5fc6260bfe17f66307a378375d7
unknown <dev.sabbirmia@gmail.com>
2024-07-08 20:25:17 +0600

    todo add

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,14 @@ onMounted(()=>{
             <div class="col-md-8 offset-md-2">
                 <h2 class="text-center">To-Do List</h2>
                 <form id="todo-form" class="form-inline mb-4">
-                    <input type="text" id="new-task" class="form-control mr-2 flex-grow-1" placeholder="Enter a new task" required>
-                    <button type="submit" class="btn btn-primary">Add Task</button>
+                    <input type="text" v-model="todoStore.todoForm.title" id="new-task" class="form-control mr-2 flex-grow-1" placeholder="Enter a new task" required>
+                    <button type="submit" class="btn btn-primary" @click.prevent="todoStore.createTodo()">Add Task</button>
                 </form>
                 <ul id="todo-list" class="list-unstyled">
                     <!-- To-Do Items will go here -->
-                     <li v-for="todo in todoStore.todos" :key="todo.id">{{todo.title}}</li>
+                     <li v-for="todo in todoStore.todos" :key="todo.id">
+                        <i class="fa-solid fa-check mr-2"></i>{{todo.title}}
+                    </li>
                 </ul>
             </div>
         </div>
--- src/stores/todo.js
+++ src/stores/todo.js
@@ -4,12 +4,27 @@ import { defineStore } from 'pinia'
 
 export const useTodoStore = defineStore('todo', {
   state:()=>({
-    todos:[]
+    todos:[],
+    todoForm:{
+      completed:false,
+      id:1,
+      title:null,
+      userId:1,
+    }
   }),
   actions:{
     async getToDos(){
       const {data}= await axios.get('https://jsonplaceholder.typicode.com/todos');
       this.todos = data;
+      
+      
     },
+    async createTodo(){
+      const {data} = await axios.post('https://jsonplaceholder.typicode.com/todos',this.todoForm);
+      this.todos.push(data);
+      this.todos.reverse();
+      
+    }
+
   }
 })
