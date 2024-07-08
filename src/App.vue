b2064a3f208aa626916449251ed7499cd568c3c7
unknown <dev.sabbirmia@gmail.com>
2024-07-08 21:48:16 +0600

    edit and upate

--- src/App.vue
+++ src/App.vue
@@ -18,12 +18,14 @@ onMounted(()=>{
                 <h2 class="text-center">To-Do List</h2>
                 <form id="todo-form" class="form-inline mb-4">
                     <input type="text" v-model="todoStore.todoForm.title" id="new-task" class="form-control mr-2 flex-grow-1" placeholder="Enter a new task" required>
-                    <button type="submit" class="btn btn-primary" @click.prevent="todoStore.createTodo()">Add Task</button>
+                    <button v-if="!todoStore.isEdit" type="submit" class="btn btn-primary" @click.prevent="todoStore.createTodo()">Add Task</button>
+                    <button v-else type="submit" class="btn btn-warning" @click.prevent="todoStore.createTodo()">Update</button>
                 </form>
                 <ul id="todo-list" class="list-unstyled">
                     <!-- To-Do Items will go here -->
                      <li v-for="todo in todoStore.todos" :key="todo.id">
-                        <i class="fa-solid fa-check mr-2"></i>{{todo.title}}
+                        <i class="fa-solid fa-trash mr-2" @click.prevent="todoStore.deleteTodo(todo.id)"></i>
+                        <i class="fa-solid fa-pen-to-square mr-2" @click.prevent="todoStore.editTodo(todo.id)"></i>{{todo.title}}
                     </li>
                 </ul>
             </div>
--- src/stores/todo.js
+++ src/stores/todo.js
@@ -10,20 +10,35 @@ export const useTodoStore = defineStore('todo', {
       id:1,
       title:null,
       userId:1,
-    }
+    },
+    isEdit:false,
+    
   }),
   actions:{
     async getToDos(){
       const {data}= await axios.get('https://jsonplaceholder.typicode.com/todos');
-      this.todos = data;
-      
-      
+      this.todos = data;      
     },
     async createTodo(){
       const {data} = await axios.post('https://jsonplaceholder.typicode.com/todos',this.todoForm);
       this.todos.push(data);
       this.todos.reverse();
-      
+      this.todoForm.title ='';
+    },
+    async deleteTodo(id){
+      const {data} = await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+      this.todos = this.todos.filter(todo=>{
+        return todo.id != id;
+      });
+    },
+    async editTodo(id){
+      const {data} = await axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`);
+      this.todoForm.title = data.title;
+      this.isEdit = true;
+    },
+    async updateTodo(id){
+      const {data} = await axios.put(`https://jsonplaceholder.typicode.com/todos/${id}`);
+      this.todos.push(data);
     }
 
   }
